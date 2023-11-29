a92405668a9408882848d7031d9e059ab09d32d5
artem <art.zavtur@gmail.com>
2023-11-29 03:55:43 +0200

    item rotate

--- lib/generated.dart
+++ lib/generated.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -61,20 +63,69 @@ class _Item extends StatefulWidget {
   State<_Item> createState() => _ItemState();
 }
 
-class _ItemState extends State<_Item> {
+class _ItemState extends State<_Item> with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+  AnimationStatus _status = AnimationStatus.dismissed;
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: const Duration(milliseconds: 500));
+    _animation = Tween(end: 1.0, begin: 0.0).animate(_controller)
+      ..addListener(() => setState(() {}))
+      ..addStatusListener((status) => _status = status);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+    _animation.removeListener(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
-      child: Center(
-        child: Text(
-          '${widget.index + 1}',
-          style: const TextStyle(
-            color: Colors.white,
-            fontSize: 24,
-          ),
-        ),
-      ),
+    return GestureDetector(
+      onTap: () {
+        if (_status == AnimationStatus.dismissed) {
+          _controller.forward();
+        } else {
+          _controller.reverse();
+        }
+      },
+      child: Transform(
+          alignment: FractionalOffset.center,
+          transform: Matrix4.identity()
+            ..setEntry(3, 2, 0.0015)
+            ..rotateY(pi * _animation.value),
+          child: _animation.value <= 0.5
+              ? Container(
+                  color: Colors.blue,
+                  child: Center(
+                    child: Text(
+                      '${widget.index + 1}',
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                )
+              : Container(
+                  color: Colors.red,
+                  child: Center(
+                    child: Transform.flip(
+                      flipX: true,
+                      child: Text(
+                        '${widget.index + 1}',
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                        ),
+                      ),
+                    ),
+                  ),
+                )),
     );
   }
 }
